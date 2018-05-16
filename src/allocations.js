028d83d737d8c0dd890a8cfcc8e12ba62a989ac2
Adrien EXCOFFIER <adrien.excoffier@epsi.fr>
2018-05-16 17:36:48 +0200

    Changes in allocations responses

--- src/allocations.js
+++ src/allocations.js
@@ -1,9 +1,6 @@
 export const allocations = [
     "Pour connaître votre égibilité et le montant de vos allocations, rendez-vous sur https://www.pole-emploi.fr/candidat/mes-allocations-@/index.jspz?id=76919",
-    "Le curé de Camaret à les couilles qui pendent, et quand il asseoit dessus, elles lui rentrent dans le cul, il bande ! IL BANDE IL BANDE IL BANDE ! https://www.pole-emploi.fr/candidat/mes-allocations-@/index.jspz?id=76919",
     "Rendez-vous sur https://www.pole-emploi.fr/candidat/mes-allocations-@/index.jspz?id=76919 afin d'obtenir plus d'informations concernant le système d'allocations",
     "Besoin d'informations concernant vos allocations, n'hésitez pas à vous rendre sur https://www.pole-emploi.fr/candidat/mes-allocations-@/index.jspz?id=76919",
-    "Sale pauvre, va bosser au lieu de réclamer, clochard de ta grande race",
-    "Y'a un poste de mendiant ouvert près des galeries Lafayette si jamais",
     "Des questions sur vos droits aux allocations ? Rendez-vous sur https://www.pole-emploi.fr/candidat/mes-allocations-@/index.jspz?id=76919",
 ]
