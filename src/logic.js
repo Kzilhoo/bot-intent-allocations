7ebcd1dc77770fe613409e3abac2a358cd011ca8
Adrien EXCOFFIER <adrien.excoffier@epsi.fr>
2018-05-16 13:15:28 +0200

    First commit

---
+++ src/logic.js
@@ -0,0 +1,12 @@
+import { allocations } from './allocations'
+
+export const logic = ($message) => {
+    return new Promise((resolve, reject) => {
+        const response = allocations[Math.floor(Math.random() * allocations.length)];
+    const newMessage = Object.assign($message.message, { content: response })
+    const newIntentMessage = Object.assign($message, { message: newMessage })
+    resolve(newIntentMessage)
+    })
+}
+
+
